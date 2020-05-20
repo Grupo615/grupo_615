e11b66cbf9bf40b2d262f4e05dad6de07b445396
MarisolStagnaro <54475593+MarisolStagnaro@users.noreply.github.com>
2020-05-20 11:39:32 -0300

    Update Tablero.java

--- codigo/app/src/main/java/com/example/tp2/Tablero.java
+++ codigo/app/src/main/java/com/example/tp2/Tablero.java
@@ -8,8 +8,8 @@ import android.graphics.Paint;
 import android.view.View;
 
 public class Tablero extends View {
-    Bitmap bitmap;
-    Bitmap tablero;
+    Bitmap bitmap,bitmap2;
+    Bitmap tablero,pausa;
     private int mHeigth;
     private int mWidth;
     Paint paint;
@@ -19,13 +19,14 @@ public class Tablero extends View {
     public Tablero(Context context){
         super(context);
         bitmap=BitmapFactory.decodeResource(getResources(),R.drawable.madera);
+        bitmap2=BitmapFactory.decodeResource(getResources(),R.drawable.pausa);
         paint=new Paint();
     }
     public void onSizeChanged(int a, int b, int c , int d){
         mHeigth=getHeight();
         mWidth =getWidth();
         tablero=Bitmap.createScaledBitmap(bitmap,mWidth,mHeigth,true);
-
+        pausa = Bitmap.createScaledBitmap(bitmap2,mWidth/4,mHeigth/8,true);
     }
     public void setPlay(boolean play){
         this.play=play;
@@ -34,6 +35,7 @@ public class Tablero extends View {
     public void onDraw(Canvas canvas){
         canvas.drawBitmap(tablero,0,0,null);
         if(!this.play)
-        canvas.drawText("PAUSA",getWidth()/2,getHeight()/2,paint);
+            canvas.drawBitmap(pausa,getWidth()/3,getHeight()/3,paint);
+            //canvas.drawText("PAUSA",getWidth()/2,getHeight()/2,paint);
     }
 }
