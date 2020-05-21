98c3eea32eca9fbb64532cfa0961586386b6a3e3
MarisolStagnaro <54475593+MarisolStagnaro@users.noreply.github.com>
2020-05-21 20:39:03 -0300

    Update LoginUsuario.java

--- codigo/app/src/main/java/com/example/tp2/LoginUsuario.java
+++ codigo/app/src/main/java/com/example/tp2/LoginUsuario.java
@@ -2,11 +2,39 @@ package com.example.tp2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class LoginUsuario extends AppCompatActivity {
+
+    public void ComprobarConexionLogin(View v) {
+        ConnectivityManager manager = (ConnectivityManager) getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo ni = manager.getActiveNetworkInfo();
+        if(ni != null && ni.isConnected())
+            irMenu(v);
+        else
+            Toast.makeText(getApplicationContext(),"No se pudo realizar el login, "+
+                    "su internet esta desconectada",Toast.LENGTH_SHORT).show();
+
+    }
+
+    public void ComprobarConexionRegistro(View v) {
+        ConnectivityManager manager = (ConnectivityManager) getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo ni = manager.getActiveNetworkInfo();
+        if(ni != null && ni.isConnected())
+            irRegistro(v);
+        else
+            Toast.makeText(getApplicationContext(),"No se pudo ingresar al registro, "+
+                    "su internet esta desconectada",Toast.LENGTH_SHORT).show();
+
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +43,8 @@ public class LoginUsuario extends AppCompatActivity {
         Intent intent= getIntent();
 
     }
+
+
     public void irMenu(View v){
         Intent intent= new Intent(LoginUsuario.this,MenuPrincipal.class);
         startActivity(intent);
@@ -24,5 +54,6 @@ public class LoginUsuario extends AppCompatActivity {
         Intent intent= new Intent(LoginUsuario.this,RegistroUsuario.class);
         startActivity(intent);
     }
+
 
 }
