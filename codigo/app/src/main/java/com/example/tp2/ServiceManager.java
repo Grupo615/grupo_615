4be252a0ae150fb142f720157bf41229fccc88e4
MarisolStagnaro <54475593+MarisolStagnaro@users.noreply.github.com>
2020-05-22 11:51:55 -0300

    Create ServiceManager.java

---
+++ codigo/app/src/main/java/com/example/tp2/ServiceManager.java
@@ -0,0 +1,19 @@
+package com.example.tp2;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+class ServiceManager {
+    Context context;
+
+    public ServiceManager(Context base) {
+        context = base;
+    }
+
+    public boolean isNetworkAvailable() {
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = cm.getActiveNetworkInfo();
+        return networkInfo != null && networkInfo.isConnected();
+    }
+}
