e524d96172947c136ed69d8734aaafeba2829b43
MarisolStagnaro <54475593+MarisolStagnaro@users.noreply.github.com>
2020-05-25 02:32:44 -0300

    Update Resultado.java

--- codigo/app/src/main/java/com/example/tp2/Resultado.java
+++ codigo/app/src/main/java/com/example/tp2/Resultado.java
@@ -3,16 +3,9 @@ package com.example.tp2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
-import android.graphics.Paint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import org.w3c.dom.Text;
 
 public class Resultado extends AppCompatActivity {
     TextView resultado;
