6e639eda244888b1a994eed00fa47856552e29b6
MarisolStagnaro <54475593+MarisolStagnaro@users.noreply.github.com>
2020-05-24 21:33:37 -0300

    Update Resultado.java

--- codigo/app/src/main/java/com/example/tp2/Resultado.java
+++ codigo/app/src/main/java/com/example/tp2/Resultado.java
@@ -3,7 +3,12 @@ package com.example.tp2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -12,6 +17,8 @@ import org.w3c.dom.Text;
 public class Resultado extends AppCompatActivity {
     TextView resultado;
     String tiempo;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,8 +29,11 @@ public class Resultado extends AppCompatActivity {
         resultado= findViewById(R.id.idResultado);
         resultado.setText(tiempo);
 
+    }
 
-
+    public void volverAJugar (View v){
+        Intent intent=new Intent(Resultado.this,MenuPrincipal.class);
+        startActivity(intent);
     }
 
 }
