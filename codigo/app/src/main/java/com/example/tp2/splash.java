1a8c95afd6b214577937a87e130182de6ab548ca
MarisolStagnaro <54475593+MarisolStagnaro@users.noreply.github.com>
2020-05-31 13:16:08 -0300

    Create splash.java

---
+++ codigo/app/src/main/java/com/example/tp2/splash.java
@@ -0,0 +1,25 @@
+package com.example.tp2;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent= new Intent(splash.this,LoginUsuario.class);
+                startActivity(intent);
+            }
+        },3000);
+    }
+}
