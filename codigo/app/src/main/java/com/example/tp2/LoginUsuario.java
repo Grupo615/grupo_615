6ee5c6441100307a90b2791308d011b0c219e830
MarisolStagnaro <54475593+MarisolStagnaro@users.noreply.github.com>
2020-05-31 16:45:24 -0300

    Update LoginUsuario.java

--- codigo/app/src/main/java/com/example/tp2/LoginUsuario.java
+++ codigo/app/src/main/java/com/example/tp2/LoginUsuario.java
@@ -1,35 +1,18 @@
 package com.example.tp2;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-
 import android.content.Context;
 import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.google.gson.Gson;
-import com.google.gson.JsonObject;
-import com.google.gson.reflect.TypeToken;
-
-import java.io.IOException;
-import java.lang.reflect.Type;
+import androidx.appcompat.app.AppCompatActivity;
 
 import RetrofitPackage.ComunicacionApiRest;
-import RetrofitPackage.ConstantesRest;
-import RetrofitPackage.ErrorResponse;
-import RetrofitPackage.InterfazRestApi;
 import RetrofitPackage.PostRegistroLogin;
-import RetrofitPackage.ResponseLogin;
-import RetrofitPackage.RestAdapter;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 
 public class LoginUsuario extends AppCompatActivity {
     private EditText email, contraseña;
@@ -49,6 +32,7 @@ public class LoginUsuario extends AppCompatActivity {
     public void ComprobarConexionLogin(View v) {
         ConnectivityManager manager = (ConnectivityManager) getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo ni = manager.getActiveNetworkInfo();
+        //Antes de logearse consultar estado de internet
         if (ni != null && ni.isConnected()) {
             logearse();
         } else
@@ -58,50 +42,19 @@ public class LoginUsuario extends AppCompatActivity {
     }
 
     public void logearse() {
-        ///   ComunicacionApiRest comunicacionApiRest = new ComunicacionApiRest(this);
         PostRegistroLogin postRegistroLogin = new PostRegistroLogin();
         postRegistroLogin.setEmail(email.getText().toString());
         postRegistroLogin.setPassword(contraseña.getText().toString());
-        //  comunicacionApiRest.enviarLogin(postRegistroLogin);
-/*
-        RestAdapter restAdapter = new RestAdapter();
-        InterfazRestApi interfazRestApi = restAdapter.conectar();
-        Call<ResponseLogin> responseLoginCall = interfazRestApi.logearse(postRegistroLogin);
-        responseLoginCall.enqueue(new Callback<ResponseLogin>() {
-            @Override
-            public void onResponse(Call<ResponseLogin> call, Response<ResponseLogin> response) {
 
-                if (response.body() == null) {
-                    Gson gson = new Gson();
-                    Type type = new TypeToken<ErrorResponse>() {
-                    }.getType();
-                    ErrorResponse errorResponse = gson.fromJson(response.errorBody().charStream(), type);
-                    Log.i("errorResponseMsg", errorResponse.getMsg());
-                    Log.i("errorResponseState", errorResponse.getState());
-                    Toast.makeText(context, errorResponse.getMsg(), Toast.LENGTH_LONG).show();
-
-                } else if (response.body().getState().equals("success")) {
-                    Toast.makeText(context, "login exitoso", Toast.LENGTH_LONG).show();
-                    ConstantesRest.setToken(response.body().getToken());
-                    irMenu();
-                }
-
-
-            }
-
-            @Override
-            public void onFailure(Call<ResponseLogin> call, Throwable t) {
-                Toast.makeText(context, t.getMessage(), Toast.LENGTH_SHORT).show();
-            }
-        });*/
-    ComunicacionApiRest comunicacionApiRest = new ComunicacionApiRest(this);
-    comunicacionApiRest.enviarLogin(postRegistroLogin,LoginUsuario.this);
+        ComunicacionApiRest comunicacionApiRest = new ComunicacionApiRest(this);
+        comunicacionApiRest.enviarLogin(postRegistroLogin,LoginUsuario.this);
 
     }
 
     public void ComprobarConexionRegistro(View v) {
         ConnectivityManager manager = (ConnectivityManager) getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo ni = manager.getActiveNetworkInfo();
+        //Antes de pasar a la activity de registro para registrarse , comprobar si hay internet
         if (ni != null && ni.isConnected())
             irRegistro(v);
         else
