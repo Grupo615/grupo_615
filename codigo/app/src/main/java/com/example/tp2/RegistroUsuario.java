e0d0a141721018a41f95333d2310ab1d57f05b61
MarisolStagnaro <54475593+MarisolStagnaro@users.noreply.github.com>
2020-05-31 16:45:28 -0300

    Update RegistroUsuario.java

--- codigo/app/src/main/java/com/example/tp2/RegistroUsuario.java
+++ codigo/app/src/main/java/com/example/tp2/RegistroUsuario.java
@@ -1,35 +1,19 @@
 package com.example.tp2;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Context;
 import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
-
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
-
-import java.lang.reflect.Type;
+import androidx.appcompat.app.AppCompatActivity;
 
 import RetrofitPackage.ComunicacionApiRest;
-import RetrofitPackage.ConstantesRest;
-import RetrofitPackage.ErrorResponse;
-import RetrofitPackage.InterfazRestApi;
-import RetrofitPackage.PostEvento;
 import RetrofitPackage.PostRegistroLogin;
-import RetrofitPackage.ResponseEvento;
-import RetrofitPackage.ResponseRegistro;
-import RetrofitPackage.RestAdapter;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 
 public class RegistroUsuario extends AppCompatActivity {
     private Button bt1;
@@ -56,9 +40,8 @@ public class RegistroUsuario extends AppCompatActivity {
     public void ComprobarConexionRegistro(View v) {
         ConnectivityManager manager = (ConnectivityManager) getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
 
-
         NetworkInfo ni = manager.getActiveNetworkInfo();
-
+        //Antes de registrar al usuario comprobamos internet
         if (ni != null && ni.isConnected()) {
             registrar();
         } else
@@ -98,85 +81,12 @@ public class RegistroUsuario extends AppCompatActivity {
         postRegistroLogin.setLastname(lastnameString);
         postRegistroLogin.setName(nameString);
         postRegistroLogin.setPassword(passwordString);
-/*
-        RestAdapter restAdapter = new RestAdapter();
 
-
-        // responseRegistro=new ResponseRegistro();
-        InterfazRestApi interfazRestApi = restAdapter.conectar();
-        Call<ResponseRegistro> responseRegistroCall = interfazRestApi.registarUsuario(postRegistroLogin);
-        responseRegistroCall.enqueue(new Callback<ResponseRegistro>() {
-            @Override
-            public void onResponse(Call<ResponseRegistro> call, Response<ResponseRegistro> response) {
-
-                if (response.body() == null) {
-                    Gson gson = new Gson();
-                    Type type = new TypeToken<ErrorResponse>() {
-                    }.getType();
-                    ErrorResponse errorResponse = gson.fromJson(response.errorBody().charStream(), type);
-                    Toast.makeText(context, errorResponse.getMsg(), Toast.LENGTH_LONG).show();
-                } else if (response.body().getState().equals("success")) {
-                    Toast.makeText(context, "Registrado correctamente", Toast.LENGTH_SHORT).show();
-                    usuarioRegistrado();
-                }
-
-
-            }
-
-            @Override
-            public void onFailure(Call<ResponseRegistro> call, Throwable t) {
-                Toast.makeText(context, "fallo la conexion", Toast.LENGTH_LONG).show();
-            }
-        });
-*/
         ComunicacionApiRest comunicacionApiRest = new ComunicacionApiRest(this);
         comunicacionApiRest.enviarPostRegitro(postRegistroLogin);
 
     }
 
-  /*  private void usuarioRegistrado() {
-       /* PostEvento postEvento= new PostEvento();
-        postEvento.setEnv("TEST");
-        postEvento.setDescription("el usuario ha sido registrado correctamente");
-        postEvento.setState("ACTIVO");
-        postEvento.setType_events("registro");
-        RestAdapter restAdapter = new RestAdapter();
-
-
-        // responseRegistro=new ResponseRegistro();
-        InterfazRestApi interfazRestApi = restAdapter.conectar();
-        Call<ResponseEvento> responseEventoCall = interfazRestApi.registrarEvento(ConstantesRest.getToken(),postEvento);
-        responseEventoCall.enqueue(new Callback<ResponseEvento>() {
-            @Override
-            public void onResponse(Call<ResponseEvento> call, Response<ResponseEvento> response) {
-                if(response.body()==null){
-                    Gson gson = new Gson();
-                    Type type = new TypeToken<ErrorResponse>() {
-                    }.getType();
-                    ErrorResponse errorResponse = gson.fromJson(response.errorBody().charStream(), type);
-                    Toast.makeText(context, errorResponse.getMsg(), Toast.LENGTH_LONG).show();
-                }
-                else  if( response.body().getState().equals("success"))
-                    Toast.makeText(context,"registro de evento exitoso",Toast.LENGTH_LONG).show();
-                else{
-                    Toast.makeText(context,"fallo el registro del evento",Toast.LENGTH_LONG).show();
-                }
-
-
-            }
-
-            @Override
-            public void onFailure(Call<ResponseEvento> call, Throwable t) {
-
-            }
-        });
-
-
-        ComunicacionApiRest comunicacionApiRest = new ComunicacionApiRest();
-        comunicacionApiRest.registrarEvento("usuario Registrado","registro");
-
-    }
-    */
 
     public void irLogin(View v) {
 
