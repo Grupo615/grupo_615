0808a9410f0a3dc55722ac3631ebf2f1bee7997b
MarisolStagnaro <54475593+MarisolStagnaro@users.noreply.github.com>
2020-05-31 16:42:52 -0300

    Update splash.java

--- codigo/app/src/main/java/com/example/tp2/splash.java
+++ codigo/app/src/main/java/com/example/tp2/splash.java
@@ -7,12 +7,11 @@ import android.os.Handler;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class splash extends AppCompatActivity {
-
+    //Pantalla de presentacion de app
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-
 
         new Handler().postDelayed(new Runnable() {
             @Override
